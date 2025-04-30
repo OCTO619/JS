d717b7c0942e0945af3463ef90a75ea50830e8b3
OCTO619 <97657572+OCTO619@users.noreply.github.com>
2025-04-30 17:20:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score=33
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)// returns NaN - not a number
+// "33"-> 33
+// "33abc"->NaN
+// true-> 1 false ->0
+// null-> 0
+// undefined-> NaN
+let isLoggedIn="ray"
+let inBoolean=Boolean(isLoggedIn)
+console.log(typeof(inBoolean))
+console.log(inBoolean)
+// ""->false (empty string)
+//"ray"->true
+let some=33
+let StringNumber=String(some)
+console.log(StringNumber)
+console.log(typeof(StringNumber))
