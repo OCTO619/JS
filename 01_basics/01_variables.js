3728eab9c30002166f4d469b73270d1246aaf4e6
OCTO619 <97657572+OCTO619@users.noreply.github.com>
2025-04-30 16:58:24 +0000

    varaiables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId= 144553
+let accountEmail= "ishi2004singh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+accountEmail="hc@hc.com"
+accountCity="bangalore"
+accountPassword="1212"
+// accountId=2 // assignment to constant variable not allowed
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var coz of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+// alert("hello")
+// alert(3+3)
+console.log(3+3); console.log("ray")
+let name="rayray" // string
+let age= 21 // numerical
+let isLoggedIn=false // boolean
+let state ; //undefined
+//let state=null
+//number 2 to power 53
+// bigint
+// null ->standalone value
+// undefined
+//symbol -? uniqueness
+// object
+console.log(typeof state)
