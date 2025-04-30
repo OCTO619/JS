437b86deec172fb114f7b24fc3effdb53cb0507d
OCTO619 <97657572+OCTO619@users.noreply.github.com>
2025-04-30 17:46:16 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score=33
+/*let score=33
 console.log(typeof score)
 console.log(typeof(score))
 let valueInNumber=Number(score)
@@ -19,3 +19,28 @@ let some=33
 let StringNumber=String(some)
 console.log(StringNumber)
 console.log(typeof(StringNumber))
+/* Operations */
+let value=3
+let negvalue=-value
+console.log(negvalue)
+console.log(2**5) // **-> raised to the power
+let str1="ray"
+let str2="mama"
+let str3=str1+str2 // cant subtract
+console.log(str3) // raymama
+console.log("1"+2) // 12
+console.log(1+"2") // 12
+console.log("1"+2+2) // 122
+console.log(1+2+"2") // 32
+console.log(+true)// 1
+console.log("") // blank 
+console.log(+"") // 0
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+--gameCounter
+console.log(gameCounter)
+let num=2
+const x=++num
+console.log(x)
+console.log(num)
