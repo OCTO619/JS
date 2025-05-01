1cb377529553d4945f56664aedaebc14d0e30fa8
OCTO619 <97657572+OCTO619@users.noreply.github.com>
2025-05-01 16:39:23 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,9 @@
+console.log("2">1) // true
+console.log(null == 0) // false
+console.log(null>0) // false
+console.log(null<0) // false
+console.log(null>=0) // true since it converts null to 0 before comparison
+console.log(undefined==0) // false
+console.log(undefined>0) // false
+console.log(undefined<0) // false
+console.log("2"===2) // absolute comparison-> not just value but datatype too
