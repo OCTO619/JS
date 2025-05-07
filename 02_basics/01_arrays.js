1edbb2b679c3504c0db0517e72ceed22be7404f0
OCTO619 <97657572+OCTO619@users.noreply.github.com>
2025-05-07 12:58:04 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,19 +1,19 @@
  const myArr=[0,1,2,3,4,5,true,"ray"]
-// console.log(myArr[0])
-// const myHeros=["thor","hulk","ironman","blackwidow"]
-// const newArr= new Array(1,2,3,4)
-// // array methods
-// myArr.push(6)
-// console.log(myArr)
-// myArr.pop()
-// myArr.unshift(0)
-// myArr.shift()
-// console.log(myArr.includes(9)) // false
-// console.log(myArr.indexOf(9)) // -1
-// const NewArr=myArr.join()
-// console.log(NewArr) // 0,1,2,3,4,5,true,ray
-// console.log(myArr)
-// console.log(typeof(NewArr)) // string
+console.log(myArr[0])
+const myHeros=["thor","hulk","ironman","blackwidow"]
+const newArr= new Array(1,2,3,4)
+// array methods
+myArr.push(6)
+console.log(myArr)
+myArr.pop()
+myArr.unshift(0)
+myArr.shift()
+console.log(myArr.includes(9)) // false
+console.log(myArr.indexOf(9)) // -1
+const NewArr=myArr.join()
+console.log(NewArr) // 0,1,2,3,4,5,true,ray
+console.log(myArr)
+console.log(typeof(NewArr)) // string
 // slice splice
 console.log("A ", myArr)
 const myn1=myArr.slice(1,3) // starting from index 1 not including 3 
